d9c08538224be642e80381fd9148da4e988930d4
Vakon <jamespagedev@outlook.com>
2023-07-12 22:54:03 -0500

    feat: add timer buttons

--- App.tsx
+++ App.tsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { BtnMain } from "@app/components";
 
 export default function App() {
+  // variables
+  const [seconds, setSeconds] = useState<number>(0);
+  const [isActive, toggleTimer] = useState(false);
+
+  // render
   return (
     <View style={styles.container}>
       <View style={styles.headerSpace} />
+      <View style={styles.btnsContainer}>
+        <BtnMain
+          name="START"
+          btnStyles={{
+            width: 80,
+            paddingHorizontal: 10,
+            paddingVertical: 16,
+            backgroundColor: "#00FF00",
+            borderRadius: 5,
+          }}
+          textStyles={{
+            color: "#000000",
+            textAlign: "center",
+            letterSpacing: 1.4,
+            fontWeight: "700",
+          }}
+          onPress={() => toggleTimer(true)}
+        />
+        <BtnMain
+          name="STOP"
+          btnStyles={{ backgroundColor: "#FF0000" }}
+          onPress={() => toggleTimer(false)}
+        />
+        <BtnMain name="RESET" onPress={() => setSeconds(0)} />
+      </View>
     </View>
   );
 }
@@ -21,5 +52,10 @@ const styles = StyleSheet.create({
   headerSpace: {
     width: "100%",
     height: 100,
+  },
+  btnsContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
